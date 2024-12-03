122e17184df2df462ace4e18a2a23d70efa591d4
Reemhossam <reemhossam1997@gmail.com>
2024-12-03 19:00:50 +0200

    learn basics in form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component, ViewChild, viewChild } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormsModule, NgForm, NgModel } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'AngularTempleteDrivenForm';
+  firstName:string = '';
+  lastName:string = '';
+  Email:string = '';
+  dob: string= '';
+
+  genderTypes = [
+    {id: 'check-male', value: 'male', display:'Male'},
+    {id: 'check-female', value: 'female', display:'Female'},
+    {id: 'check-other', value: 'other', display:'Prefer not to say'}
+    ];
   @ViewChild('registerationForm')  registerationForm:NgForm;
+  @ViewChild('uname') uname:NgModel;
   onFormSubmitted(){
     console.log(this.registerationForm);
+    console.log(this.registerationForm.controls['firstname'].value);
+    console.log(this.registerationForm.value.lastname);
+  }
+  GenerateUserName(){
+    console.log(this.firstName+this.lastName+this.dob.substr(0,4));
+    //this.uname.setvalue=
+    console.log(this.uname.value);
   }
 }
